c33d95ceac3e0bc41b3d08d5e81ec33cb243f3a9
Basil Suter <git@nadar.io>
2016-11-01 10:57:25 +0100

    cleanup the block interface, add internal base block teste, added concret implementation block closes #1063

--- src/base/BlockInterface.php
+++ src/base/BlockInterface.php
@@ -2,41 +2,145 @@
 
 namespace luya\cms\base;
 
+/**
+ * Interface for all Blocks.
+ * 
+ * The below methods are required in order to create your own block abstraction layer.
+ * 
+ * @author Basil Suter <basil@nadar.io>
+ */
 interface BlockInterface
 {
-    // user base input/config methods
-
-    // public function extraVars();
-
-    // public function name();
-
-    // public function config();
-
-    // luya based methods to access
-
-    // public function renderFrontend();
-
-    // public function renderAdmin();
-
-    public function getFieldHelp();
-
-    public function setVarValues(array $values);
-
-    public function setCfgValues(array $values);
-
-    public function setEnvOption($key, $value);
+    // block methods defintions implementations
     
-    // block unspecific methods
-
-    public function renderFrontend();
-    
-    public function renderAdmin();
-    
+    /**
+     * Get the name of the block in order to display in administration context.
+     */
     public function name();
     
-    public function config();
+    /**
+     * Returns the icon based on material icon names
+     * 
+     * @return string
+     */
+    public function icon();
     
-    public function extraVars();
+    /**
+     * Get the output in the frontend context.
+     *
+     * @return string
+     */
+    public function renderFrontend();
     
-    public function addExtraVar($key, $value);
+    /**
+     * Get the output in administration context.
+     *
+     * @return string
+     */
+    public function renderAdmin();
+    
+    /**
+     * Returns a class of the blocks group.
+     *
+     * @return \luya\cms\base\BlockGroup
+     */
+    public function getBlockGroup();
+    
+    // getters & setters from outside
+    
+    /**
+     * The the full name based on icon() and name() concated.
+     *
+     * @return string
+     */
+    public function getFullName();
+    
+    /**
+     * Returns an array with additional help informations for specific field (var or cfg).
+     *
+     * @return array An array where the key is the cfg/var field var name and the value the helper text.
+     */
+    public function getFieldHelp();
+    
+    /**
+     * Set an environment option informations to the block with key value pairing.
+     * 
+     * @param string $key The identifier key.
+     * @param mixed $value The value for the key.
+     */
+    public function setEnvOption($key, $value);
+    
+    /**
+     * Set the values for element vars with an array key value binding.
+     * 
+     * @param array $values An array where key is the name of the var-element and value the content.
+     */
+    public function setVarValues(array $values);
+    
+    /**
+     * Set the values for element cfgs with an array key value binding.
+     *
+     * @param array $values An array where key is the name of the cfg-element and value the content.
+     */
+    public function setCfgValues(array $values);
+    
+    /**
+     * Set the value from placeholders where the array key is the name of value the content of the placeholder.
+     *
+     * @param array $placeholders An array with placeholders where key is name and the value the content e.g. `['content' => 'The placheholder Content']`.
+     */
+    public function setPlaceholderValues(array $placeholders);
+    
+    /**
+     * Returns an array of key value pairing with additional informations to pass to the API and frontend.
+     * 
+     * @return array
+     */
+    public function extraVarsOutput();
+   
+    /**
+     * Returns all config vars element of key value pairing to pass to the API and frontend.
+     * 
+     * @return array
+     */
+    public function getVars();
+   
+    /**
+     * Returns all config cfgs element of key value pairing to pass to the API and frontend.
+     *
+     * @return array
+     */
+    public function getCfgs();
+    
+    /**
+     * Returns all config placeholders element of key value pairing to pass to the API and frontend.
+     * 
+     * @return array
+     */
+    public function getPlaceholders();
+
+    /**
+     * Whether cache is enabled for this block or not.
+     * 
+     * @return boolean
+     */
+    public function getIsCacheEnabled();
+    
+    /**
+     * The time of cache expiration
+     */
+    public function getCacheExpirationTime();
+    
+    /**
+     * Whether is an container element or not.
+     */
+    public function getIsContainer();
+    
+    /**
+     * Return an array of assets
+     * 
+     * @todo remove in rc1
+     * @return array
+     */
+    public function getAssets();
 }
