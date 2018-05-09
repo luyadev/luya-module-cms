951f8cf5d8f0d1558f2ea5e5317d749f886b4fe8
Basil Suter <git@nadar.io>
2018-05-09 13:58:57 +0200

    basic ngrest test for block group

---
+++ tests/CmsNgRestTestCase.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace cmstests;
+
+
+use luya\testsuite\cases\NgRestTestCase;
+
+class CmsNgRestTestCase extends NgRestTestCase
+{
+    public function getConfigArray()
+    {
+        return [
+            'id' => 'ngresttest',
+            'basePath' => dirname(__DIR__),
+            'components' => [
+                'db' => [
+                    'class' => 'yii\db\Connection',
+                    'dsn' => 'sqlite::memory:',
+                ]
+            ]
+        ];
+    }
+}
---
+++ tests/src/models/BlockGroupTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tests\web\cmsadmin\models;
+
+use cmstests\CmsNgRestTestCase;
+
+class BlockGroupTest extends CmsNgRestTestCase
+{
+    public $modelClass = 'luya\cms\models\BlockGroup';
+    
+    public $apiClass = 'luya\cms\admin\apis\BlockgroupController';
+    
+    public $controllerClass = 'luya\cms\admin\controllers\BlockgroupController';
+}
