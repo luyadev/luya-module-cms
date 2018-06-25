92bcc08c993cb62976251228d035ad2970d6a1a2
Basil Suter <git@nadar.io>
2018-06-25 13:30:53 +0200

    php docs about event

--- src/frontend/events/BeforeRenderEvent.php
+++ src/frontend/events/BeforeRenderEvent.php
@@ -3,14 +3,23 @@
 namespace luya\cms\frontend\events;
 
 /**
- * An event will be triggered before the rendering of cms controller content happends.
+ * Before Render Event.
+ * 
+ * An event will be triggered before the rendering of cms controller content.
  *
  * @author Basil Suter <basil@nadar.io>
  * @since 1.0.0
  */
 class BeforeRenderEvent extends \yii\base\Event
 {
+    /**
+     * @var boolean Whether current request is valid or not, if $isValid is false an {{MethodNotAllowedHttpException}} will
+     * be thrown while rendering.
+     */
     public $isValid = true;
     
+    /**
+     * @var \luya\cms\menu\Item The current menu item resolved from {{luya\cms\Menu::getCurrent()}}.
+     */
     public $menu;
 }
