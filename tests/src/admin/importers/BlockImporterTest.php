17a9502e9712ae33b9655af9db74f738f9a321fd
Basil <git@nadar.io>
2019-06-18 17:45:12 +0200

    fix unit test

--- tests/src/admin/importers/BlockImporterTest.php
+++ tests/src/admin/importers/BlockImporterTest.php
@@ -74,14 +74,19 @@ class BlockImporterTest extends CmsConsoleTestCase
         $this->assertNull($importer->run());
         
         $log = $importer->importer->getLog();
-        
+
+        // remove 4 key because of path
+
+        $pathKey = $log['luya\cms\admin\importers\BlockImporter'][4];
+        unset($log['luya\cms\admin\importers\BlockImporter'][4]);
+
+        $this->assertContains('Unable to find', $pathKey);
         $this->assertSame([
             'luya\cms\admin\importers\BlockImporter' => [
                 0 => 'Insert new block group block_group_dev_elements.',
                 1 => 'block \luya\cms\frontend\blocks\HtmlBlock: Added to database',
                 2 => 'Insert new block group block_group_dev_elements.',
                 3 => 'block \luya\cms\frontend\blocks\ModuleBlock: Added to database',
-                4 => 'Unable to find \'notfoundatall\' in any of those paths \'/var/www/luya-module-cms,\'',
                 5 => 'Block importer finished with 2 blocks.',
             ]
         ], $log);
