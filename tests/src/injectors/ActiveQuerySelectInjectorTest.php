90f97e1a310a771647d2d5fb0f4387c7b431fd9d
Basil Suter <git@nadar.io>
2019-07-11 14:05:05 +0200

    active query select injeftor

---
+++ tests/src/injectors/ActiveQuerySelectInjectorTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace cmstests\src\injectors;
+
+use cmstests\ModelTestCase;
+use luya\cms\injectors\ActiveQuerySelectInjector;
+use luya\testsuite\fixtures\NgRestModelFixture;
+use luya\cms\models\Layout;
+use cmstests\data\blocks\import\TestBlock;
+
+class ActiveQuerySelectInjectorTest extends ModelTestCase
+{
+    public function testVariableResponse()
+    {
+        $fixture = new NgRestModelFixture([
+            'modelClass' => Layout::class,
+            'fixtureData' => [
+                1 => [
+                    'id' => 1,
+                    'name' => 'foo',
+                    'json_config' => '{"node":"value"}',
+                    'view_file' => 'none.php',
+                ]
+            ]
+        ]);
+
+        $block = new TestBlock();
+
+        $block->setVarValues([
+            'foobar' => 1,
+        ]);
+
+        $injector = new ActiveQuerySelectInjector([
+            'query' => Layout::find(),
+            'varName' => 'foobar',
+            'varLabel' => 'test',
+            'label' => 'name',
+            'context' => $block,
+        ]);
+        $injector->setup();
+
+
+        $f = $block->getExtraValue('foobar');
+        $x = $block->getVarValue('foobar');
+        $y = $block->getConfigVarsExport();
+        
+        $this->assertSame('foo', $y[0]['options'][0]['label']);
+        $this->assertSame(1, $x);
+        $this->assertSame(['node' => 'value'], $f->getJsonConfig());
+        $this->assertSame('value', $f->getJsonConfig('node'));
+    }
+}
