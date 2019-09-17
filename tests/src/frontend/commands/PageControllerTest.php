8063cbc9c11fddd13288e3fc4bd163ba68e61658
Basil Suter <git@nadar.io>
2019-09-17 16:49:13 +0200

    test abort by user

--- tests/src/frontend/commands/PageControllerTest.php
+++ tests/src/frontend/commands/PageControllerTest.php
@@ -3,7 +3,6 @@
 namespace cmstests\src\frontend\commands;
 
 use cmstests\CmsConsoleTestCase;
-use luya\cms\frontend\blocks\HtmlBlock;
 use luya\cms\frontend\commands\PageController;
 use luya\testsuite\scopes\PageScope;
 use luya\testsuite\traits\CommandStdStreamTrait;
@@ -33,6 +32,30 @@ class PageControllerTest extends CmsConsoleTestCase
             $r = $ctrl->actionCleanup();
             
             $this->assertSame(0, $r);
+        });
+
+
+        PageScope::run($this->app, function(PageScope $scope) {
+            $scope->createCmsNavFixture([]);
+            $scope->createCmsNavItemFixture([]);
+            $scope->createCmsNavItemPageFixture([]);
+            $scope->createCmsNavItemPageBlockItemFixture([
+                'item1' => [
+                    'id' => 1,
+                    'json_config_values' => '',
+                    'json_config_cfg_values' => '',
+                    'block_id' => 0,
+                    'placeholder_var' => 'default',
+                    'nav_item_page_id' => 1,
+                ]
+            ]);
+            $scope->createCmsPropertyFixture([]);
+            $scope->createCmsNavPermissionFixture([]);
+            $ctrl = new PageControllerStub('page', $this->app);
+            $ctrl->truncateStreams();
+            $r = $ctrl->actionCleanup();
+            
+            $this->assertSame(1, $r);
         });
     }
 }
