a033b578ba4f3445d74d377cb0e51d474af91586
boehsermoe <boehsermoe@me.com>
2019-12-17 07:56:30 +0100

    testing toolbar rendering

--- tests/src/controllers/DefaultControllerTest.php
+++ tests/src/controllers/DefaultControllerTest.php
@@ -2,11 +2,22 @@
 
 namespace cmstests\src\controllers;
 
+use luya\admin\models\Lang;
 use luya\cms\frontend\blocks\HtmlBlock;
+use luya\cms\models\Nav;
+use luya\cms\models\NavContainer;
+use luya\cms\models\NavItem;
 use luya\cms\models\NavItemPage;
+use luya\cms\models\Property;
 use luya\testsuite\cases\WebApplicationTestCase;
+use luya\testsuite\fixtures\ActiveRecordFixture;
 use luya\testsuite\scopes\PageScope;
-use Yii;
+use yii\base\Event;
+
+class ControllerStub extends \luya\cms\frontend\base\Controller
+{
+
+}
 
 class DefaultControllerTest extends WebApplicationTestCase
 {
@@ -22,9 +33,13 @@ class DefaultControllerTest extends WebApplicationTestCase
             ],
             'components' => [
                 'db' => [
-                        'class' => 'yii\db\Connection',
-                        'dsn' => 'sqlite::memory:',
-                    ]
+                    'class' => 'yii\db\Connection',
+                    'dsn' => 'sqlite::memory:',
+                ],
+                'menu' => 'luya\cms\Menu',
+                'composition' => [
+                    'default' => ['langShortCode' => 'en']
+                ]
             ]
         ];
     }
@@ -43,5 +58,96 @@ class DefaultControllerTest extends WebApplicationTestCase
             $copy = (string) $content;
             $this->assertNotNull($content);
         });
+    }
+    
+    public function testRenderToolbar()
+    {
+        //region Fictures
+        
+        $langFixture = new ActiveRecordFixture([
+            'modelClass' => Lang::class,
+            'fixtureData' => [
+                'en' => [
+                    'id' => 1,
+                    'short_code' => 'en',
+                    'is_default' => 1,
+                ],
+            ]
+        ]);
+        
+        $langFixture = new ActiveRecordFixture([
+            'modelClass' => NavContainer::class,
+            'fixtureData' => [
+                [
+                    'id' => 1,
+                    'name' => 'default',
+                    'alias' => 'default',
+                ],
+            ]
+        ]);
+        
+        $navFixture = new ActiveRecordFixture([
+            'modelClass' => Nav::class,
+            'fixtureData' => [
+                [
+                    'id' => 1,
+                    'is_home' => 1,
+                    'is_hidden' => 0,
+                    'is_offline' => 0,
+                    'nav_container_id' => 1,
+                    'parent_nav_id' => 0,
+                    'is_draft' => 0,
+                    'is_deleted' => 0,
+                ]
+            ]
+        ]);
+    
+        $navItemFixture = new ActiveRecordFixture([
+            'modelClass' => NavItem::class,
+            'fixtureData' => [
+                [
+                    'id' => 1,
+                    'nav_id' => 1,
+                    'lang_id' => 1,
+                    'title' => 'en',
+                    'alias' => 'en-slug',
+                    'nav_item_type' => NavItem::TYPE_PAGE,
+                    'nav_item_type_id' => 1,
+                ]
+            ]
+        ]);
+    
+        $navItemFixture = new ActiveRecordFixture([
+            'modelClass' => Property::class,
+        ]);
+        //endregion
+    
+        $this->app->menu->currentUrlRule = [
+            'route' => 'default/default',
+            'params' => [
+                'slug' => 'default',
+            ],
+        ];
+    
+        $controller = new ControllerStub('default', $this->app->getModule('cms'));
+        $controller->view->context = $controller;
+        
+        $event = new Event([
+            'sender' => $controller->view,
+            'data' => ['content' => 'FOO']
+        ]);
+    
+        ob_start();
+        $controller->renderToolbar($event);
+        $toolbarHtml = ob_get_clean();
+    
+        $internalErrors = libxml_use_internal_errors(true);
+    
+        $doc = new \DOMDocument();
+        $loaded = $doc->loadHTML($toolbarHtml);
+    
+        libxml_use_internal_errors($internalErrors);
+    
+        $this->assertTrue($loaded);
     }
 }
