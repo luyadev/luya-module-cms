0a8dd8dd25cd64393eea9ea6ff17eee6912302fe
Basil Suter <git@nadar.io>
2020-10-06 07:14:51 +0000

    remove info about not translated page, its in the toast messages already

--- src/admin/views/page/_navitem.php
+++ src/admin/views/page/_navitem.php
@@ -177,7 +177,6 @@ use luya\helpers\Html;
     </div>
 </div>
 <div class="cmsadmin-page" ng-if="!isTranslated && loaded">
-    <div class="alert alert-info"><?= Module::t('view_update_no_translations'); ?></div>
     <div ng-controller="CopyPageController">
         <h3><?= Module::t('view_index_add_page_from_language'); ?></h3>
         <p><?= Module::t('view_index_add_page_from_language_info'); ?></p>
