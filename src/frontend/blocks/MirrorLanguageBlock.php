65083bfd6864ee2af8cd2f7d4a6f692bc5876031
Basil Suter <git@nadar.io>
2020-10-15 13:00:14 +0000

    rename block, add translations

--- src/frontend/blocks/MirrorLanguageBlock.php
+++ src/frontend/blocks/MirrorLanguageBlock.php
@@ -5,12 +5,14 @@ namespace luya\cms\frontend\blocks;
 use luya\admin\models\Lang;
 use luya\cms\base\PhpBlock;
 use luya\cms\frontend\blockgroups\DevelopmentGroup;
+use luya\cms\frontend\Module;
 use luya\cms\helpers\BlockHelper;
 use luya\cms\models\NavItem;
 use luya\cms\models\NavItemPage;
 use luya\cms\models\NavItemPageBlockItem;
+use Yii;
 
-class MirrorContentBlock extends PhpBlock
+class MirrorLanguageBlock extends PhpBlock
 {
     /**
      * @var string The module where this block belongs to in order to find the view files.
@@ -30,7 +32,7 @@ class MirrorContentBlock extends PhpBlock
      */
     public function name()
     {
-        return 'Mirror Page';
+        return Module::t('block_mirror_language_name');
     }
     
     /**
@@ -38,7 +40,7 @@ class MirrorContentBlock extends PhpBlock
      */
     public function icon()
     {
-        return 'compare'; // see the list of icons on: https://design.google.com/icons/
+        return 'compare';
     }
  
     /**
@@ -48,27 +50,34 @@ class MirrorContentBlock extends PhpBlock
     {
         return [
             'vars' => [
-                 ['var' => 'language', 'label' => 'Language', 'type' => self::TYPE_SELECT, 'options' => BlockHelper::selectArrayOption(Lang::find()->select(['name', 'id'])->indexBy('id')->column())],
+                 ['var' => 'language', 'label' => Module::t('block_mirror_config_language_label'), 'type' => self::TYPE_SELECT, 'options' => BlockHelper::selectArrayOption(Lang::find()->select(['name', 'id'])->indexBy('id')->column())],
             ],
         ];
     }
     
     /**
-     * {@inheritDoc} 
-     *
-     * @param {{vars.language}}
+     * {@inheritDoc}
     */
     public function admin()
     {
         if (empty($this->getVarValue('language'))) {
-            return '<div>Configure a <b><span class="material-icons">edit</span> language</b> to mirror its content for the current placeholder.</div>';
+            return '<div>'.Module::t('block_mirror_admin_empty_language').'</div>';
         }
 
-        return '<div>Mirroring this placeholder from <span class="material-icons">arrow_right_alt</span> <b>'.Lang::find()->where(['id' => $this->getVarValue('language')])->select(['name'])->scalar().'</b>.';
+        return '<div>'.Module::t('block_mirror_admin_configured_language', ['name' => Lang::find()->where(['id' => $this->getVarValue('language')])->select(['name'])->scalar()]).'</div>';
     }
 
+    /**
+     * {@inheritDoc}
+    */
     public function frontend()
     {
+        $langId = $this->getVarValue('language');
+        
+        if ($langId == Yii::$app->adminLanguage->activeId) {
+            return Yii::debug('Circular content mirroring detected.', __METHOD__);
+        }
+
         $item = NavItemPageBlockItem::find()
             ->select(['placeholder_var', 'nav_item_page_id'])
             ->where(['id' => $this->getEnvOption('id')])
@@ -78,8 +87,6 @@ class MirrorContentBlock extends PhpBlock
         $navItemId = NavItemPage::find()->where(['id' => $item['nav_item_page_id']])->select(['nav_item_id'])->scalar();
         
         $navId = NavItem::find()->where(['id' => $navItemId])->select(['nav_id'])->scalar();
-
-        $langId = $this->getVarValue('language');
 
         $navItem = NavItem::find()->where(['nav_id' => $navId, 'nav_item_type' => NavItem::TYPE_PAGE, 'lang_id' => $langId])->one();
 
