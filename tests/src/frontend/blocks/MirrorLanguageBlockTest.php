f3e978abe803a17bb073f8c024c44a47c3570407
Basil Suter <git@nadar.io>
2020-10-15 15:43:35 +0000

    test tables

--- tests/src/frontend/blocks/MirrorLanguageBlockTest.php
+++ tests/src/frontend/blocks/MirrorLanguageBlockTest.php
@@ -5,16 +5,63 @@ namespace cmstests\src\frontend\blocks;
 use cmstests\ModelTestCase;
 use luya\cms\frontend\blocks\MirrorLanguageBlock;
 use luya\testsuite\traits\AdminDatabaseTableTrait;
+use luya\testsuite\traits\CmsDatabaseTableTrait;
 
 class MirrorLanguageBlockTest extends ModelTestCase
 {
-    use AdminDatabaseTableTrait;
+    use CmsDatabaseTableTrait;
 
     public function testRenderFrontend()
     {
-        $this->createAdminLangFixture();
+        $this->createAdminLangFixture([
+            1 => [
+                'id' => 1,
+                'name' => 'en',
+                'short_code' => 'en',
+                'is_default' => 1,
+                'is_deleted' => 0,
+            ],
+            2 => [
+                'id' => 2,
+                'name' => 'de',
+                'short_code' => 'de',
+                'is_default' => 0,
+                'is_deleted' => 0,
+            ]
+        ]);
+        $this->createCmsNavItemPageBlockItemFixture([
+            1 => [
+                'id' => 1,
+                'placeholder_var' => 'xyz',
+                'nav_item_page_id' => 1,
+            ]
+        ]);
+        $this->createCmsNavItemPageFixture([
+            1 => [
+                'id' => 1,
+                'nav_item_id' => 1
+            ]
+        ]);
+
+        $this->createCmsNavItemFixture([
+            1 => [
+                'id' => 1,
+                'lang_id' => 1,
+            ],
+            2 => [
+                'id' => 2,
+                'lang_id' => 2,
+            ]
+        ]);
+
         $block = new MirrorLanguageBlock();
 
         $this->assertEmpty($block->frontend());
+
+        $block->setVarValues(['language' => 1]);
+
+        $r = $block->frontend();
+
+        var_dump($r);
     }
 }
