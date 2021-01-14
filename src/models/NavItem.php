efe008bfd9d9c26f1b3218f279aaca192318a946
Basil Suter <git@nadar.io>
2021-01-14 13:13:56 +0000

    fix issues with pgsql

--- src/models/NavItem.php
+++ src/models/NavItem.php
@@ -10,6 +10,7 @@ use yii\base\Exception;
 use luya\cms\admin\Module;
 use luya\admin\models\Lang;
 use luya\admin\base\GenericSearchInterface;
+use luya\admin\helpers\Angular;
 use luya\admin\models\User;
 use luya\helpers\Inflector;
 
@@ -310,6 +311,18 @@ class NavItem extends ActiveRecord implements GenericSearchInterface
             'timestamp_update' => time(),
             'update_user_id' => Module::getAuthorUserId(),
         ]);
+    }
+
+    public function fields()
+    {
+        $fields = parent::fields();
+        $fields['is_cacheable'] = function($model) {
+            return Angular::typeCast($model->is_cacheable);
+        };
+        $fields['is_url_strict_parsing_disabled'] = function($model) {
+            return Angular::typeCast($model->is_url_strict_parsing_disabled);
+        };
+        return $fields;
     }
 
     /* GenericSearchInterface */
