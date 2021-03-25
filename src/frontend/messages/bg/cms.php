3345de855c187911fab06079ecc9809392c0fbea
testt23 <rekstomi@abv.bg>
2021-03-25 16:03:33 +0200

    cms.php
    Language Bulgarian

---
+++ src/frontend/messages/bg/cms.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Language: Bulgarian
+Translate by Vl. Vladimirov
+25.03.2021
+*/
+return [
+    'tb_composition' => 'Език',
+    'tb_properties' => 'Свойства',
+    'tb_seo' => 'Оптимизация за търсачки',
+    'tb_seo_title' => 'Заглавие',
+    'tb_seo_description' => 'Описание',
+    'tb_seo_description_notfound' => 'Към тази страница все още няма добавено описание.',
+    'tb_seo_link' => 'URL връзка',
+    'tb_seo_keywords' => 'Ключови думи',
+    'tb_seo_keywords_notfound' => 'Няма намерени ключови думи. Трябва да добавите ключови думи, за да анализирате съдържанието си. ',
+    'tb_seo_warning' => 'Някои от ключовите Ви думи не са намерени във вашето съдържание, трябва да коригирате това, като промените ключовите си думи или добавите липсващите ключови думи към съдържанието.',
+    'tb_edit_alt' => 'Редактиране на тази страница в CMS администриране',
+    'tb_visible_not_alt' => 'Тази страница НЕ се вижда за посетители',
+    'tb_visible_alt' => 'Тази страница е видима за посетители',
+    'block_html_html_label' => 'HTML код',
+    'block_html_no_content' => 'Все още не е добавен HTML код.',
+    'block_module_name' => 'Модул',
+    'block_module_modulename_label' => 'Име на модула',
+    'block_module_modulecontroller_label' => 'Име на контролера (без суфикс на контролера)',
+    'block_module_moduleaction_label' => 'Име на действие (без префикс за действие)',
+    'block_module_moduleactionargs_label' => 'Аргументи за действие (json: {"var": "value"})',
+    'block_module_no_module' => 'Все още не е посочен модул.',
+    'block_module_integration' => 'Интегриране на модул',
+    'block_html_name' => 'HTML',
+    'block_module_modulename_help' => 'Изброени са само модули от фронтенд, които са регистрирани в конфигурационния файл.',
+    'block_group_dev_elements' => 'Разработка',
+    'block_group_layout_elements' => 'Оформление',
+    'block_group_basic_elements' => 'Основни положения',
+    'block_group_project_elements' => 'Проект',
+    'block_group_text_elements' => 'Текстове',
+    'block_group_media_group' => 'Медия',
+    
+    // 1.0.0
+    'block_module_strictrender' => 'Строго изобразяване',
+    'block_module_strictrender_help' => 'Когато е разрешено стриктно изобразяване, модулът ще изпълнява само предоставения маршрут (модул, контролер, действие, параметри), без да слуша маршрути за действие и контролер.',
+    'block_html_cfg_raw_label' => 'Показване на HTML в администратор',
+
+    // 3.4.0
+    'tag_alias_readme' => 'Псевдонимът ви позволява да използвате псевдоними, дефинирани във вашето приложение, както и предварително дефинирани псевдоними. Като пример можете да използвате `псевдоним [@web]` за свързване към изображения в публичната html папка: `<img src="alias[@web]/image.jpg">`',
+	'tag_menu_readme' => 'Генериране на връзка към елемент от меню, където ключът е идентификаторът на страницата (можете да видите идентификаторите на страниците, когато задържите курсора на мишката върху навигацията в сайта в администрацията).',
+	'tag_page_readme' => 'Вземете съдържанието на цяла страница или на заместител на страница. Първият параметър е идентификаторът на страницата (който получавате, като задържите курсора на мишката върху навигацията на сайта в администрацията): `page [1]`. Ако искате да получите само съдържанието на заместител на cmslayout, използвайте втория параметър: `page [1] (placeholderName)`. ',
+	'block_mirror_language_name' => 'Огледален език',
+	'block_mirror_config_language_label' => 'Изходен език',
+	'block_mirror_admin_empty_language' => 'Конфигуриране на <b> <span class="material-icons"> редактиране </span> език на изход </b>, за да отразява съдържанието му за текущото запазено място.',
+	'block_mirror_admin_configured_language' => 'Огледално копиране на това запазено място от <span class="material-icons">arrow_right_alt</span> <b> {name} </b>.',
+];
