a2697808f1ac261dbad79594138b5807e8ec942d
Basil Suter <git@nadar.io>
2021-06-03 11:59:16 +0000

    phpdocs

--- src/frontend/events/MenuItemEvent.php
+++ src/frontend/events/MenuItemEvent.php
@@ -2,36 +2,53 @@
 
 namespace luya\cms\frontend\events;
 
+use luya\cms\menu\Item;
+
 /**
- * Event after Menu component is loaded and ready.
+ * Event after a menu item is loaded and ready.
  *
  * Each Menu Item does have en event which will be trigger after create. You can also access
  * this event in your config like this for instance:
  *
  * ```php
  * 'menu' => [
- *     'class' => 'cms\menu\Container',
- *     'on menuItemEvent' => function($event) {
+ *     'class' => 'luya\cms\Menu',
+ *     'on eventOnItemFind' => function(MenuItemEvent $event) {
  *         if ($event->item->alias == 'this-is-my-alias') {
  *             // will turn this item to invisble
  *             $event->visible = false;
  *         }
  *     }
  *  ],
- *  ```
+ * ```
+ * 
+ * @property integer $visible Menu item visibility getter/setter.
  *
  * @author Basil Suter <basil@nadar.io>
  * @since 1.0.0
  */
 class MenuItemEvent extends \yii\base\Event
 {
+    /**
+     * @var Item
+     */
     public $item;
     
+    /**
+     * Whether the item is visible or not
+     *
+     * @return boolean
+     */
     public function getVisible()
     {
         return !$this->item->isHidden;
     }
     
+    /**
+     * Setter method for the item visibility
+     *
+     * @param integer $state
+     */
     public function setVisible($state)
     {
         $this->item->isHidden = !$state;
