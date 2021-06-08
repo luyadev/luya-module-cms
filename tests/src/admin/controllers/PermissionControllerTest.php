8eb751a8c897c6a951c9688e00fb7810df73fc6b
Basil Suter <git@nadar.io>
2021-06-08 08:05:29 +0000

    just do some syntax error check

---
+++ tests/src/admin/controllers/PermissionControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace cmstests\src\admin\controllers;
+
+use luya\testsuite\scopes\PermissionScope;
+use cmstests\WebModelTestCase;
+use luya\cms\admin\controllers\DefaultController;
+use luya\cms\admin\controllers\PermissionController;
+
+class PermissionControllerTest extends WebModelTestCase
+{
+    public function testActions()
+    {
+        $ctrl = new PermissionController('permission', $this->app->getModule('cmsadmin'));
+        $this->assertNotEmpty($ctrl->actionIndex());
+    }
+}
