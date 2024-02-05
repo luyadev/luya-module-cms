4f5d83fecad04dd1392ac394bf8cf6894002f9b9
Hendrik Bugdoll <bugdoll@appsoluteinfach.de>
2024-02-05 18:15:12 +0100

    Added new unit tests for block envOptions

--- tests/src/base/BlockTest.php
+++ tests/src/base/BlockTest.php
@@ -5,6 +5,7 @@ namespace tests\web\cmsadmin\base;
 use cmstests\CmsFrontendTestCase;
 use cmstests\data\blocks\TestBlock;
 use luya\cms\base\PhpBlock;
+use yii\db\ActiveRecord;
 
 class GetterSetter extends PhpBlock
 {
@@ -39,9 +40,8 @@ class BlockTest extends CmsFrontendTestCase
     {
         $block = new TestBlock();
 
-        $this->assertEquals(false, $block->isAdminContext());
-        $this->assertEquals(false, $block->isFrontendContext());
-
+        $this->assertFalse($block->isAdminContext());
+        $this->assertFalse($block->isFrontendContext());
 
         foreach ($block->getConfigVarsExport() as $var) {
             $this->assertArrayHasKey('id', $var);
@@ -68,14 +68,47 @@ class BlockTest extends CmsFrontendTestCase
     {
         $block = new TestBlock();
 
-        $block->setEnvOption('blockId', 1);
-        $block->setEnvOption('context', 'admin');
-
         $block->setVarValues(['var1' => 'content var 1', 'var2' => 'content var 2']);
         $block->setCfgValues(['cfg1' => 'content cfg 1']);
 
         $this->assertEquals('content var 1', $block->admin()[0]);
         $this->assertEquals('content var 2', $block->admin()[1]);
+        $this->assertEquals('content cfg 1', $block->admin()[2]);
+    }
+
+    public function testBlockEnvOptions()
+    {
+        $block = new TestBlock();
+        $pageObject = new ActiveRecord(); // @TODO specify class
+
+        $block->setEnvOption('id', 1);
+        $block->setEnvOption('blockId', 1);
+        $block->setEnvOption('context', 'admin');
+        $block->setEnvOption('pageObject', $pageObject);
+        $block->setEnvOption('index', 1);
+        $block->setEnvOption('itemsCount', 1);
+        $block->setEnvOption('isFirst', true);
+        $block->setEnvOption('isLast', true);
+        $block->setEnvOption('isPrevEqual', false);
+        $block->setEnvOption('isNextEqual', false);
+        $block->setEnvOption('equalIndex', 1);
+
+        $this->assertTrue($block->isAdminContext());
+        $this->assertFalse($block->isFrontendContext());
+
+        $envOptions = $block->admin()[3];
+        $this->assertSame(1, $envOptions['id']);
+        $this->assertSame(1, $envOptions['blockId']);
+        $this->assertSame('admin', $envOptions['context']);
+        // @TODO assertInstanceOf() for pageObject
+        $this->assertNotEquals(false, $envOptions['pageObject']);
+        $this->assertSame(1, $envOptions['index']);
+        $this->assertSame(1, $envOptions['itemsCount']);
+        $this->assertTrue($envOptions['isFirst']);
+        $this->assertTrue($envOptions['isLast']);
+        $this->assertFalse($envOptions['isPrevEqual']);
+        $this->assertFalse($envOptions['isNextEqual']);
+        $this->assertSame(1, $envOptions['equalIndex']);
     }
 
     public function testGetterSetter()
